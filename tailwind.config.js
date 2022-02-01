f515a31e2ee011a56d5e105a50d7334c428e4370
brekk <brekk@brekkbockrath.com>
2022-02-01 13:39:56 -0800

    countdown timer

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,6 +90,9 @@ module.exports = {
           DEFAULT: '#FFC2EB',
           subtext: '#D089B9',
         },
+        ifmauve: {
+          DEFAULT: '#9B6288',
+        },
         ifcubepink: {
           DEFAULT: '#FF94DB',
         },
